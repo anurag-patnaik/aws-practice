19c7f99245082dcbdb08e7fce9b11e1cd74787d9
Anurag Patnaik <anurag.patnaik@autodesk.com>
2021-07-21 11:54:34 -0700

    Adding express router. Added /api/ping endpoint.
    Returns ip address detected in incoming request.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
+router.get('/api/ping', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log(ip);
+
+  res.json({
+    ip
+  });
+});
+
+app.use('/', router);
 app.listen(3000, () => {
   console.log('[server]: AWS express app is running on port 3000.');
 });
 
-app.get('/api/1', (req, res) => {
-  const respObj = {
-    'data': 1
-  };
-
-  res.json(respObj);
-});
-
-app.get('/api/2', (req, res) => {
-  const respObj = {
-    'data': 2
-  };
-
-  res.json(respObj);
-});
+// app.get('/api/1', (req, res) => {
+//   const respObj = {
+//     'data': 1
+//   };
+//
+//   res.json(respObj);
+// });
+//
+// app.get('/api/2', (req, res) => {
+//   const respObj = {
+//     'data': 2
+//   };
+//
+//   res.json(respObj);
+// });
