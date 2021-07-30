2c243949eec11e459b6b74f08094d3ac7fb474ac
Anurag Patnaik <anurag.patnaik@autodesk.com>
2021-07-30 10:49:49 -0700

    Adding babel transpiling to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     externalsPresets: { node: true },
-    externals: [nodeExternals()]
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    }
 }
